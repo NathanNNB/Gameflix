9e5148d4a39e7915dd88f8c9ee382fe14a15ba71
NathanNNB <nathan-borges2011@live.com>
2020-09-23 11:21:38 -0300

    Att link backend

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://oneflixnnb.herokuapp.com';
+  : 'https://gameflixnn.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
